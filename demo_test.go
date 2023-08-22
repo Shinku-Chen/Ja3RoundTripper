3854fd96df9374ccc92639d8b62f2d5b1ab6984e
Shinku <chenzhuyu@xd.com>
2023-08-22 12:06:50 +0800

    init

--- demo_test.go
+++ demo_test.go
@@ -3,14 +3,17 @@ package Ja3RoundTripper
 import (
 	"io"
 	"net/http"
+	"net/url"
 	"testing"
 )
 
 func TestRoundTripper(t *testing.T) {
+	u, _ := url.Parse("http://127.0.0.1:8888")
+
 	c, _ := (&http.Client{
 		Transport: &Ja3RoundTripper{
-
-			Ja3: "771,4865-4866-4867-49196-49195-52393-49200-49199-52392-49162-49161-49172-49171-157-156-53-47-49160-49170-10,0-23-65281-10-11-16-5-13-18-51-45-43-27-21,29-23-24-25,0",
+			Proxy: http.ProxyURL(u),
+			Ja3:   "771,4865-4866-4867-49196-49195-52393-49200-49199-52392-49162-49161-49172-49171-157-156-53-47-49160-49170-10,0-23-65281-10-11-16-5-13-18-51-45-43-27-21,29-23-24-25,0",
 			//HeaderOrder:     nil,
 			//	Timeout:         0,
 			//DisableRedirect: false,
